e2d7e324ca34bbd0b3ff2bf396acb189a1d540be
리코타치즈 <133624013+lico0531@users.noreply.github.com>
2025-03-08 19:51:07 +0900

    Add files via upload

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -11,9 +11,7 @@ const Welcome = () => {
 
   return (
     <main className="welcome p-5 mt-6 text-white items-center justify-center text-center text-2xl break-keep">
-      <h2 className="font-semibold">React와 Firebase를 활용한 채팅 앱 예제입니다.</h2>
-      <p>상단 메뉴의 로그인 버튼을 눌러 구글 로그인을 진행해주세요.</p>
-      <p>로그인이 성공하면 채팅 화면으로 자동으로 이동됩니다.</p>
+      <h2 className="font-semibold">React와 Firebase를 활용한 졸작 웹/앱 배포 연습중....</h2>
       <p>Finale 화이팅</p>
       <p>졸작 웹/앱 배포 연습중....</p>
     </main>
